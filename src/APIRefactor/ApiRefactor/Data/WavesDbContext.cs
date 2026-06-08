5e2f4b3f226d74459b9e2c7683385550a50de1d8
Sheetal <gajare.sheetal@gmail.com>
2026-06-08 21:05:52 +1000

    added Wave to DB context

--- src/APIRefactor/ApiRefactor/Data/WavesDbContext.cs
+++ src/APIRefactor/ApiRefactor/Data/WavesDbContext.cs
@@ -1,11 +1,44 @@
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using ApiRefactor.Models;
 
 namespace ApiRefactor.Data;
 
 public class WavesDbContext : DbContext
 {
     private const string ConnectionString = "Data Source=App_Data/waves.db";
+
+    public DbSet<Wave> Waves { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite(ConnectionString);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure Wave entity
+        modelBuilder.Entity<Wave>()
+            .HasKey(w => w.Id);
+
+        modelBuilder.Entity<Wave>()
+            .Property(w => w.Id)
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Wave>()
+            .Property(w => w.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        modelBuilder.Entity<Wave>()
+            .Property(w => w.WaveDate)
+            .IsRequired();
+    }
 
     public SqliteConnection GetConnection()
     {
