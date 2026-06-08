bbfc64b044c1970f9999595b01ec092c2743cc67
Sheetal <gajare.sheetal@gmail.com>
2026-06-08 21:06:53 +1000

    Model to add key

--- src/APIRefactor/ApiRefactor/Models/Wave.cs
+++ src/APIRefactor/ApiRefactor/Models/Wave.cs
@@ -1,14 +1,11 @@
 namespace ApiRefactor.Models;
 
-using System.ComponentModel.DataAnnotations;
-
 /// <summary>
 /// Represents a Wave entity.
 /// This model contains only data properties and no data access logic.
 /// </summary>
 public class Wave
 {
-    [Key]
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public DateTime WaveDate { get; set; } = DateTime.Now;
